0318a9d92b63e0c7c6472ab29e8c0f5630449dd8
amejid <abdelmejid89@gmail.com>
2025-02-18 11:29:46 +0300

    Implement persistence operations

--- main.go
+++ main.go
@@ -1,16 +1,24 @@
 package main
 
 import (
-	"github.com/go-chi/chi/v5"
-	"github.com/go-chi/cors"
-	"github.com/joho/godotenv"
+	"database/sql"
 	"log"
 	"net/http"
 	"os"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/cors"
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
+
+	"github.com/amejid/rssagg/internal/database"
 )
 
-func main() {
+type apiConfig struct {
+	DB *database.Queries
+}
 
+func main() {
 	err := godotenv.Load(".env")
 	if err != nil {
 		return
@@ -20,6 +28,18 @@ func main() {
 	if portString == "" {
 		log.Fatal("PORT is not found in the environment")
 	}
+
+	dbURL := os.Getenv("DB_URL")
+	if dbURL == "" {
+		log.Fatal("DB_URL is not found in the environment")
+	}
+
+	conn, err := sql.Open("postgres", dbURL)
+	if err != nil {
+		log.Fatal("Cannot connect to DB")
+	}
+
+	apiCfg := apiConfig{DB: database.New(conn)}
 
 	router := chi.NewRouter()
 
@@ -35,6 +55,7 @@ func main() {
 	v1Router := chi.NewRouter()
 	v1Router.Get("/healthz", handlerReadiness)
 	v1Router.Get("/err", handlerErr)
+	v1Router.Post("/users", apiCfg.handlerCreateUser)
 
 	router.Mount("/v1", v1Router)
 
